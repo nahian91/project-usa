ffd14f07b6130c063d8bf7939499f5ccf854448d
nahian28 <nahiansylhet@gmail.com>
2023-01-29 11:59:09 +0600

    Updates

--- project-single.php
+++ project-single.php
@@ -19,7 +19,7 @@
     <div class="container">
         <div class="grid">
             <div class="col-8">
-                <img class="single-project__featured" src="assets/img/project-1.jpg" alt="">
+                <img class="single-project__featured" src="assets/img/service-details.jpg" alt="">
                 <p class="single-project__list">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                 <div class="grid">
                    <div class="col-12">
--- service-single.php
+++ service-single.php
@@ -27,13 +27,17 @@
                     <li class="single-service__item"><a href="service-single.php">QA Testing</a></li>
                     <li class="single-service__item"><a href="service-single.php">Product Design</a></li>
                 </ul>
+                <div class="single-service__promo" style="background-image: url('assets/img/cherrydeck-rMILC1PIwM0-unsplash.jpg');">
+                    <span class="single-service__promoo-sub">contact us now</span>
+                    <h4 class="single-service__promoo-title">you need help?</h4>
+                    <a href="" class="btn btn-primary">get quote</a>
+                </div>
             </div>
             <div class="col-8">
                 <img src="assets/img/service-details.jpg" class="single-service__img" alt="">
                 <h4 class="single-service__title">Service Overview</h4>
                 <p class="single-service__desc">Lorem ipsum is simply free text used by copytyping refreshing. Neque porro est qui dolorem ipsum quia quaed inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Aelltes port lacus quis enim var sed efficitur turpis gilla sed sit amet finibus eros. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ndustry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 <p class="single-service__desc">When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-                <br>
                 <h4 class="single-service__title">Service Features</h4>
                 <p class="single-service__desc">Lorem ipsum is simply free text used by copytyping refreshing. Neque porro est qui dolorem ipsum quia quaed inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
                 <ul class="single-service__features">
